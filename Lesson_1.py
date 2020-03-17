ab5c48132d7e9c567314eaa69868e0828bbbbc66
mirdinemris <mirdinemris@mail.ru>
2020-03-17 12:06:06 +0300

    Добавил Чукепуксов

--- Lesson_1.py
+++ Lesson_1.py
@@ -2,28 +2,34 @@ import requests
 import json
 import random
 
-term1 = random.randrange(0, 9)
-term2 = random.randrange(0, 9)
-symbols = ['-', '+']
-symbol = random.choice(symbols)
-print("Введите капчу!")
-print(term1, symbol, term2, )
-c = int(input('Введите Ответ ='))
-if symbol == '-':
-    a = term1 - term2
-    if a == c:
-      print('Доступ разрешен')
-    elif a is not c:
-      print('Доступ отклонен')
+a = random.randrange(1, 5)
+b = random.randrange(1, 5)
+c = random.randrange(6, 9)
+while b != c:
+    if a != c:
+        term1 = random.randrange(0, 9)
+        term2 = random.randrange(0, 9)
+        symbols = ['-', '+']
+        symbol = random.choice(symbols)
+        print("\033[37m♣ Капча! ♣")
+        print(term1, symbol, term2, )
+        c = int(input('Введите Ответ ='))
+        if symbol == '+':
+                b = term1 + term2
+                if b != c:
+                  print('\033[31m♦ В Доступе отказано ♦')
+        elif symbol == '-':
+               a = term1 - term2
+               if a != c:
+                 print('\033[31m♦ В Доступе отказано ♦')
+        continue
+    else:
+     break
 
-elif symbol == '+' :
-    b = term1 + term2
-    if b == c:
-      print('Доступ разрешен')
-    elif b is not c:
-      print('Доступ отклонен')
-      
-      url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+print('\033[32m ♥ Доступ_разрешен ♥')
+url = 'https://www.cbr-xml-daily.ru/daily_json.js'
 response = requests.get(url)
 data = json.loads(response.text)
+print('Последние новости из страны Чукепуксов!')
 print(data)
+
